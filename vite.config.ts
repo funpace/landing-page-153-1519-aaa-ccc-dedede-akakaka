6591ca6b0e607540a50dbab5ffe7aa742a5240d2
Robson Cavalcante <robmacav@gmail.com>
2026-02-22 23:39:41 -0400

    chore: gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
-      base: "/landing-page",
+      base: "/landing-page-153-1519-aaa-ccc-dedede-akakaka",
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
